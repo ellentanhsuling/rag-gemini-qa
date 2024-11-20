a0c9ef95717cb256abc2c412e9c7bcaee8f2c683
Ellen <ellentanhsuling@gmail.com>
2024-11-20 21:24:44 +0800

    Added SQLite configuration for Streamlit Cloud compatibility

--- rag_app.py
+++ rag_app.py
@@ -1,3 +1,9 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# Rest of your imports below
+
 import os
 import streamlit as st
 from dotenv import load_dotenv
